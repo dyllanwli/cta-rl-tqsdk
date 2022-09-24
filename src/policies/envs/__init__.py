3cb4853120b1aa1b5ce11a746ea6c8724a4b3576
dyllanwli <messsking@gmail.com>
2022-09-24 11:36:20 -0500

    add envs

---
+++ src/policies/envs/__init__.py
@@ -0,0 +1,7 @@
+from gym.envs.registration import register
+
+
+register(
+    id="FuturesEnv-v1",
+    entry_point="env:FuturesEnvV1"
+)
---
+++ src/policies/envs/futures_env_v1.py
@@ -0,0 +1,12 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+
+class FuturesEnvV1(gym.Env):
+    """
+    Custom Environment with Tqsdk for RL training
+    """
+    def __init__(self, ):
+        pass
+    
