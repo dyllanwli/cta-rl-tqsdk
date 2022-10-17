b766e1eca169d6caf6e3af0bb03285b8559b9bf5
dyllanwli <messsking@gmail.com>
2022-10-17 10:15:06 -0500

    update commodity

--- src/dao_downloader.py
+++ src/dao_downloader.py
@@ -10,20 +10,15 @@ from tqsdk import TqApi
 import wandb
 
 def main():
-    tqAPI = API(account='a4')
-    # tqAPI.test()
-    # cmod = Commodity()
-    # symbol = cmod.get_instrument_name('egg')
+    tqAPI = API(account='a3')
     dao = MongoDAO()
     # dao.download_data(tqAPI.auth, ['cotton', 'methanol'], date(2016, 10, 25), date(2022, 2, 1))
     # dao.download_data(tqAPI.auth, ['rebar'], date(2016, 10, 25), date(2022, 2, 1))
-    intervals = {'1m', '1d'}
-    dao.download_data(tqAPI.auth, ['soybean', "cotton"], date(2018, 6, 20), date(2022, 9, 1), intervals)
-    # init wandb by symbol and datetime
-    # wandb.init(project="tqrl-dev",
-            #    name=datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
-    # rl_trainer = RLTrainer(auth=tqAPI.auth)
-    # rl_trainer.train()
-
+    intervals = {'1s', '5s', '1m', '1d'}
+    symbol_list = ['soybean_oil']
+    dao.download_data(tqAPI.auth, symbol_list, date(2016, 1, 1), date(2022, 9, 1), intervals)
+    print(symbol_list, intervals, "downloaded")
 if __name__ == "__main__":
     main()
+
+
