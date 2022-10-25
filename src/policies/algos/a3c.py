ef7a7f66c1db122541b68eec80e2cfe411258d4a
dyllanwli <messsking@gmail.com>
2022-10-25 16:32:55 -0500

    aa

--- src/policies/algos/a3c.py
+++ src/policies/algos/a3c.py
@@ -22,7 +22,7 @@ class A3CConfig:
             "use_gae": True,
             "lambda": 0.5,
             "grad_clip": 40.0,
-            "lr": tune.grid_search([5e-07, 2e-07]),
+            "lr": tune.grid_search([5e-07, 2e-07]) if is_tune else 2e-07,
             # "lr_schedule": [[0, 5e-05], [100, 1e-05]],
             "vf_loss_coeff": 0.5,
             "entropy_coeff": tune.grid_search([0.2, 0.1, 0.01]) if is_tune else 0.01, # to avoid suboptimal policy
--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,2 +1,3 @@
-pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
 conda env create -f environment.yml
+conda activate quant
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
